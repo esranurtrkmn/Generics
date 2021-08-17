74a1d63c29438ca73f70e045e874998814b9895f
Esranur Türkmen <34512770+esranurtrkmn@users.noreply.github.com>
2021-08-17 23:01:55 +0300

    .

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> persons = new MyDictionary<int, string>();
+
+            persons.Add(1, "Esra");
+            persons.Add(2, "Yusuf");
+            persons.Listed();
+        }
+    }
+}
